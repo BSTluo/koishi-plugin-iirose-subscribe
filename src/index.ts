6d903bb8119712b04ee8b74d789063c8af368860
BSTluo <1946831552@qq.com>
2025-02-23 11:12:25 +0800

    更新版本号至0.0.3，添加adminList配置以限制订阅功能的使用权限

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import { } from 'koishi-plugin-adapter-iirose';
 
 export const name = 'iirose-subscribe';
 
-export interface Config { }
+export interface Config
+{
+  adminList: string[];
+}
 
 export const inject = ['database'];
 
-export const Config: Schema<Config> = Schema.object({});
+export const Config: Schema<Config> = Schema.object({
+  adminList: Schema.array(String).description('管理员uid列表')
+});
 
 declare module 'koishi' {
   interface Tables
@@ -27,14 +32,15 @@ const usage = `# 快速说明
 2. 在群内发送iirose.sub.off，将会取消订阅BOT的消息
 3. 订阅状态会在数据库中保存，不会因为重启而消失
 4. 本插件只会在iirose中生效
+5. adminList为管理员列表，需要填写用户的唯一标识(如:5b0fe8a3b1ff2)，只有管理员才能使用订阅功能
 
 # 注意事项
 开启订阅状态后，私聊bot发送内容，会被转发到群内
 开启订阅状态后，群内发送内容，会被转发到私聊bot
 关闭订阅状态后，不会再转发消息
-`
+`;
 
-export function apply(ctx: Context)
+export function apply(ctx: Context, config: Config)
 {
   ctx.model.extend('iirose_subscribe', {
     uid: 'string',
@@ -48,6 +54,9 @@ export function apply(ctx: Context)
   ctx.command("iirose.sub", '订阅房间').subcommand('.on', '开启订阅').action(async (v, message) =>
   {
     if (v.session.platform !== 'iirose') { return ' [IIROSE-Subscribe] 该平台不支持使用此插件'; }
+
+    if (!config.adminList.includes(v.session.userId)) { return ' [IIROSE-Subscribe] 你没有权限使用此功能'; }
+
     const userData = await ctx.database.get('iirose_subscribe', v.session.userId);
 
     if (userData.length > 0 && userData[0].status)
@@ -74,6 +83,9 @@ export function apply(ctx: Context)
   ctx.command("iirose.sub", '订阅房间').subcommand('.off', '关闭订阅').action(async (v, message) =>
   {
     if (v.session.platform !== 'iirose') { return ' [IIROSE-Subscribe] 该平台不支持使用此插件'; }
+
+    if (!config.adminList.includes(v.session.userId)) { return ' [IIROSE-Subscribe] 你没有权限使用此功能'; }
+
     const userData = await ctx.database.get('iirose_subscribe', v.session.userId);
 
     if (userData.length > 0 && !userData[0].status)
@@ -96,14 +108,12 @@ export function apply(ctx: Context)
       return ` [IIROSE-Subscribe] 将 [*${v.session.username}*] 设置为BOT取消订阅状态`;
     }
   });
-  
+
   ctx.on('message', async (session) =>
   {
     if (session.platform !== 'iirose') { return; }
 
     const userData = await ctx.database.get('iirose_subscribe', session.userId);
-
-    if (userData.length <= 0 || !userData[0].status) return;
 
     const msg = session.content;
     const channelId = session.channelId;
@@ -111,7 +121,6 @@ export function apply(ctx: Context)
     if (channelId.startsWith('public'))
     {
       const listTemp = await ctx.database.get('iirose_subscribe', { status: true });
-      // const list = listTemp.filter(v => { if (v.uid !== session.userId) return true; else return false; }).map(v => v.uid);
       const list = listTemp.map(v => v.uid);
       list.forEach(v =>
       {
@@ -119,9 +128,10 @@ export function apply(ctx: Context)
       });
     } else if (channelId.startsWith('private'))
     {
+      if (userData.length <= 0 || !userData[0].status) return;
+
       session.bot.sendMessage('public', ` [*${session.username}*] ： ${msg}`);
       const listTemp = await ctx.database.get('iirose_subscribe', { status: true });
-      // const list = listTemp.filter(v => { if (v.uid !== session.userId) return true; else return false; }).map(v => v.uid);
       const list = listTemp.map(v => v.uid);
       list.forEach(v =>
       {
